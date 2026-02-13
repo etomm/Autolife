53a308f7b873ef04fbfe3c7ee1e9b73acf275c0f
etomm <etomm@users.noreply.github.com>
2026-02-13 10:44:22 +0100

    Remove unnecessary theme.js file

--- src/Autolife.Web/wwwroot/js/theme.js
+++
@@ -1,7 +0,0 @@
-// Theme detection helper
-window.matchMedia = function(query) {
-    if (query === '(prefers-color-scheme: dark)') {
-        return window.matchMedia(query).matches;
-    }
-    return false;
-};
